1719ebfb4ef60cce96215fa3fb2af74bb2549216
jgottschick <jgottschick@1544eac5-21f3-0310-bb41-f67cc755567e>
2005-05-12 08:41:36 +0000

    The files for the MiddleKit will be generated by the build file

--- Trunk/Middle/GeneratedSQL/InsertIDs.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO IDs(Tablename, LastID) VALUES ('AccountsVE',0);
-INSERT INTO IDs(Tablename, LastID) VALUES ('AccountsBK',1100);
-INSERT INTO IDs(Tablename, LastID) VALUES ('AccountsVV',0);
